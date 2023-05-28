a6e35fe3d1ae854cd7074332015ba68bed4610d8
YoussefAboelwafa <youssef.hossam49@gmail.com>
2023-05-28 03:47:24 +0300

    Heap class

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(10,20,15,12,40,25,18));
         Heap h = new Heap(numbers);
-
         h.printArr();
     }
 }
